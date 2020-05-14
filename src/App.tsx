740331aa35e564e9c2e303ecdfd1892608dfea82
Maurício A. Baú <mauricio.a.bau@gmail.com>
2020-05-13 22:21:47 -0300

    API de Contexto

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/globals';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
-    {/* <SignUp /> */}
+    <AuthContext.Provider value={{ name: 'Juca' }}>
+      <SignIn />
+      {/* <SignUp /> */}
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
